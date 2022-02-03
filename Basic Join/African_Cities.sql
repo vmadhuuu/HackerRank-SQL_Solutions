31416d861fe7b646636af192a9dd43b31db0f934
Madhumitha Venkatesan <71954642+vmadhuuu@users.noreply.github.com>
2022-02-03 14:17:59 +0530

    Update African_Cities.sql

--- Basic Join/African_Cities.sql
+++ Basic Join/African_Cities.sql
@@ -1,1 +1,3 @@
-select city.name from city join country on city.countrycode = country.code where country.continent = 'Africa';
+select City.name from City inner join Country
+On Countrycode = Code
+Where Continent = 'Africa';
